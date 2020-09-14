9b12e72a744f13925c808b2308266d0fc0099504
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-14 19:56:55 +0300

    Updates

---
+++ precache-manifest.0e0f832a2f86fc5776d38bdf07d9db3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922a402583cada00f5e8c4a9fb07e6e1",
+    "url": "/dataviz-scatterplot-d3/index.html"
+  },
+  {
+    "revision": "78766ecb3efb53cd64ee",
+    "url": "/dataviz-scatterplot-d3/static/css/main.71023571.chunk.css"
+  },
+  {
+    "revision": "80b151c294765ec72a76",
+    "url": "/dataviz-scatterplot-d3/static/js/2.78500963.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dataviz-scatterplot-d3/static/js/2.78500963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78766ecb3efb53cd64ee",
+    "url": "/dataviz-scatterplot-d3/static/js/main.3dccac2b.chunk.js"
+  },
+  {
+    "revision": "29436ee75a6e6f75be47",
+    "url": "/dataviz-scatterplot-d3/static/js/runtime-main.98d48cdb.js"
+  }
+]);
